3297030e7fc5823c6965d0540b69d14592a5186b
Rudy Rigot <rudy.rigot@gmail.com>
2017-02-24 22:36:41 -0600

    Etude 4 crée depuis étude 3

---
+++ etude_4/elections/model/interfaces.go
@@ -0,0 +1,8 @@
+package model
+
+// Reader contains all the functions to read the necessary data
+type Reader interface {
+	AllPoliticians() (Politicians, error)
+	PoliticianFromID(ID int) (Politician, error)
+	AllVotes() (Votes, error)
+}
---
+++ etude_4/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"os/user"
+
+	"github.com/D-BookeR/LivreGo-sources/etude_4/elections/helper"
+	"github.com/D-BookeR/LivreGo-sources/etude_4/elections/model"
+)
+
+func main() {
+
+	// Building a slice of the name of each vote data file, so that we can pass it in the model
+	votesFileNames := []string{}
+	for i := 0; i < 100; i++ {
+		votesFileNames = append(votesFileNames, fmt.Sprintf("votes_%d.json", i+1))
+	}
+
+	// A cross-platform way to get the user's home directory, so that we can pass it in the model
+	usr, err := user.Current()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Creating the model struct
+	m := model.FromFiles{DirPath: usr.HomeDir + "/LivreGo-data", PoliticiansFileName: "politicians.json", VotesFileNames: votesFileNames}
+
+	// Getting all votes
+	allVotes, err := m.AllVotes()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	//
+	r := helper.ComputeRound(allVotes)
+
+	// Delete blanc
+	delete(r, 0)
+
+	//
+	w, err := r.Winner(&m)
+	if err != nil {
+		fmt.Println("ERROR")
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Printf("The winner is %s!\n", w)
+
+	fmt.Println(len(allVotes))
+}
